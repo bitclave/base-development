f1bd6f4b1520d36da7d02bebaeff7903274c01f5
Sai Boorlagadda <sai.boorlagadda@gmail.com>
2018-09-25 21:40:32 -0700

    no curl in start

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 docker-compose up -d
 
 # Wait for base-node to start
-until [ "`curl --silent --show-error --connect-timeout 1 -I http://localhost:8080 | grep 'Content-Type'`" != "" ];
-do
-  echo --- sleeping for 10 seconds
-  sleep 10
-done
+# until [ "`curl --silent --show-error --connect-timeout 1 -I http://localhost:8080 | grep 'Content-Type'`" != "" ];
+# do
+#   echo --- sleeping for 10 seconds
+#   sleep 10
+# done
 
 echo 'base-node is ready!'
