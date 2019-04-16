42dcd674898b68ef6e62ab4edfcadd3d0ebca057
Mark Shwartzman <mark@bitclave.com>
2019-04-16 16:42:45 -0700

    fix script

--- git-update-to-ssh.sh
+++ git-update-to-ssh.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+git submodule init
+git config submodule.base-node.url git@github.com:bitclave/base-node.git
+git config submodule.base-matcher.url git@github.com:bitclave/base-matcher.git
+git config submodule.base-rt-search.url git@github.com:bitclave/base-rt-search.git
+git config submodule.base-auth-frontend.url git@github.com:bitclave/base-auth-frontend.git
+git config submodule.base-auth-sdk.url git@github.com:bitclave/base-auth-sdk.git
+git config submodule.shepherd-backend.url git@github.com:bitclave/shepherd-backend.git
+git config submodule.shepherd.url git@github.com:bitclave/shepherd.git
+git submodule update
+
 pushd base-node
 git checkout develop
 git pull origin develop
