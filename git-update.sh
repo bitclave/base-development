fa235f561f01d6875b5c96b3b3fff89d539580ef
Mark Shwartzman <mark@bitclave.com>
2019-04-16 14:53:23 -0700

    update to latest

--- git-update.sh
+++ git-update.sh
@@ -1,29 +1,36 @@
 #!/bin/sh
 
 pushd base-node
+git checkout develop
 git pull origin develop
 popd
 
 pushd base-matcher
+git checkout master
 git pull origin master
 popd
 
 pushd base-rt-search
+git checkout master
 git pull origin master
 popd
 
 pushd base-auth-frontend
+git checkout master
 git pull origin master
 popd
 
 pushd base-auth-sdk
+git checkout master
 git pull origin master
 popd
 
 pushd shepherd-backend
+git checkout master
 git pull origin master
 popd
 
 pushd shepherd
+git checkout master
 git pull origin master
 popd
